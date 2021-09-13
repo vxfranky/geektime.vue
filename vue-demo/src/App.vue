65a8339aa80c1536733eea84b22364888a753a83
vxfranky <vxfranky@gmail.com>
2021-09-13 22:28:51 +0800

    Replace global components with single-file components

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,28 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <mark>使用 slot 来定义插槽，分类：默认插槽、具名插槽、作用域插槽</mark><br>
+    <mark>默认插槽：组件嵌套中实际上就是使用了子组件的 template 作为默认插槽</mark><br>
+    <mark>具名插槽：带有名称的 template </mark><br>
+    <mark>作用域插槽：可以接收子组件传递的值 </mark><br>
+    <mark>定义的插槽未在模板中使用时，将使用默认值，尝试删除第二个 template 查看效果</mark><br>
+    <todo-list>
+        <todo-item v-for="item in items" v-bind:key="item.title" @delete="handleDelete" :title="item.title"
+            :del="item.del">
+            <!-- SLOT SYNTAX - Vue 2.5 and earlier
+            <span slot="prefix">PREFIX</span>
+            <span slot="suffix">SUFFIX</span>
+            -->
+            <!-- SLOT SYNTAX - Vue 2.6+ -->
+            <template v-slot:prefix>
+                <span>PREFIX</span>
+            </template>
+            <template v-slot:suffix="{value}">
+                <span>SUFFIX {{value}}</span>
+            </template>
+        </todo-item>
+    </todo-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
+import TodoItem from './components/TodoItem.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    TodoList,
+    TodoItem
+  },
+  data() {
+    return {
+      items: [{
+                title: 'Course 1',
+                del: true
+              }, {
+                  title: 'Course 2',
+                  del: false
+              }]
+    } 
+  },
+  methods: {
+      handleDelete(val) {
+          console.log('Delete item', val)
+      }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ vue-demo/src/components/TodoItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <li>
+    <slot name="prefix"></slot>
+    <span v-if="!del"> {{ title }} </span>
+    <span class="red" v-else style="text-decoration: line-through"> {{ title }} </span>
+    <slot name="suffix" :value="value">DEFAULT</slot>
+    <button v-show="!del" @click="handleClick">Delete</button>
+  </li>
+</template>
+<script>
+export default {
+  props: {
+    title: String,
+    del: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data: function () {
+    return {
+      value: Math.random(),
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log("You have clicked delete button.");
+      this.$emit("delete", this.title);
+    },
+  }
+}
+</script>
+<style scoped>
+.red {
+  color: red
+}
+</style>
